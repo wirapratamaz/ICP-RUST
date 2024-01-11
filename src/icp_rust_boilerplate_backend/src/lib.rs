ab9b8c597eb855e0986ed1922adcb86b79709562
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-01-11 23:32:47 +0800

    feat: add importing dependencies and simple explain

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -1,4 +1,14 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+// Import dependencies
+#[macro_use]
+// serde is framework for serializing and deserializing Rust data structures efficiently and generically
+extern crate serde;
+// import candid so we can use the Encode and Decode traits
+use candid::{Decode, Encode};
+// import the IC SDK so we can use the ic_cdk::export::candid macro
+use ic_cdk::api::time;
+// the library use set of data structures that are stable over time
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
+// std is the standard library of Rust
+use std::{borrow::Cow, cell::RefCell};
+
