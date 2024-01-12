f39fce55a7903600185e801385fb87526aa4510e
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-01-12 16:18:00 +0800

    Feat: add Storable-BoundedStorable traits for Message struct, set up thread local and message payload

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -27,6 +27,48 @@ struct Message {
     updated_at: Option<u64>,
 }
 
+// a rait that must be implement for a struct that is stored in a stable struct
+impl Storable for Message {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+// another trait tat mut be define max size of the struct and whether it is fixed size
+impl BoundedStorable for Message {
+    const MAX_SIZE: u32 = 1024;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+// Set up thread local variables
+thread_local! {
+    // variable holds canister virtual memory
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init(DefaultMemoryImpl::default())
+    );
+
+    // variable holds canisster ID counter, which is used to generate unique IDs for messages   
+    static ID_COUNTER: RefCell<IdCell> = RefCell::new(
+        IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), 0)
+            .expect("Cannot create a counter")
+    );
+
+    // variable holds canister storage, enabling access from anywhere
+    static STORAGE: RefCell<StableBTreeMap<u64, Message, Memory>> =
+        RefCell::new(StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+    ));
+}
 
 
-
+// set up MessagePayload
+#[derive(candid::CandidType, Serialize, Deserialize, Default)]
+struct MessagePayload {
+    title: String,
+    body: String,
+    attachment_url: String,
+}
